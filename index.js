c5c9613fdee22a3a6074c2953989669304b2a027
hieu <hieu@example.com>
2024-03-10 07:43:26 +0700

    create layout client

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const routes = require("./routes/client/indexroutes.js");
 const app = express();
 const port = process.env.PORT;
-const database=require("./config/connect.js");
+const database = require("./config/connect.js");
 //dung de cau hinh views trong giao dien
 app.set("views", "./views");
 app.set("view engine", "pug");
 //end dung de cau hinh pug trong giao dien
 app.use(express.static("public"));
 //dung de nhung file tinh vao
-
-
-
 
 //routes
 routes(app);
